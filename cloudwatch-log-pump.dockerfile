bea27566e82212a74aa49b38433c7d98258d0aac
Hamed Iravanchi <iravanchi@gmail.com>
2021-01-08 15:43:57 -0800

    Add empty project with pieces of sample code

---
+++ cloudwatch-log-pump.dockerfile
@@ -0,0 +1,14 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:3.1 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:3.1 AS build
+WORKDIR /src
+COPY . .
+RUN dotnet restore cloudwatch-log-pump.sln
+RUN dotnet build ./src/CloudWatchLogPump/CloudWatchLogPump.csproj -c Release --no-restore -o /app/build 
+RUN dotnet publish ./src/CloudWatchLogPump/CloudWatchLogPump.csproj -c Release -o /app/publish
+ 
+FROM base AS final
+WORKDIR /app
+COPY --from=build /app/publish .
+ENTRYPOINT ["dotnet", "CloudWatchLogPump.dll"]
---
+++ src/CloudWatchLogPump/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.CloudWatchLogs;
+using Amazon.CloudWatchLogs.Model;
+
+namespace CloudWatchLogPump
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await DescribeSubscriptionFilters();
+        }
+
+        public static async Task DescribeSubscriptionFilters()
+        {
+            IAmazonCloudWatchLogs client = new AmazonCloudWatchLogsClient(RegionEndpoint.CACentral1);
+
+            string nextToken = null;
+            
+            do
+            {
+                var logs = await client.FilterLogEventsAsync(new FilterLogEventsRequest()
+                {
+                    LogGroupName = "log_group_name",
+                    StartTime = DateTimeOffset.UtcNow.AddMinutes(-5).ToUnixTimeMilliseconds(),
+                    NextToken = nextToken
+                });
+                
+                logs.Events.ForEach(e =>
+                {
+                });
+
+                if (logs.Events != null && logs.Events.Any())
+                    Console.WriteLine($"Received {logs.Events.Count} events " +
+                                      $"from {DateTimeOffset.FromUnixTimeMilliseconds(logs.Events[0].Timestamp):s} " +
+                                      $"to {DateTimeOffset.FromUnixTimeMilliseconds(logs.Events[^1].Timestamp):s} ");
+                
+                nextToken = logs.NextToken;
+
+            } while (!string.IsNullOrWhiteSpace(nextToken));
+        }
+
+        private static async Task UseDescribeLogStreams(IAmazonCloudWatchLogs client)
+        {
+            string nextToken = null;
+
+            for (int i = 0; i < 5; i++)
+            {
+                var describeLogStreamsRequest = new DescribeLogStreamsRequest()
+                {
+                    LogGroupName = "log_group_name",
+                    NextToken = nextToken,
+                    OrderBy = OrderBy.LastEventTime,
+                    Descending = true
+                };
+
+                var describeLogStreamsResult = await client.DescribeLogStreamsAsync(describeLogStreamsRequest);
+                foreach (var stream in describeLogStreamsResult.LogStreams)
+                    Console.WriteLine(stream.LogStreamName);
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                if (string.IsNullOrWhiteSpace(describeLogStreamsResult.NextToken))
+                    break;
+
+                nextToken = describeLogStreamsResult.NextToken;
+            }
+        }
+    }
+}
